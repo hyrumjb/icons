d0dc2fe955d7920f8434802f2ec3955c64a1731f
Hyrum Bradshaw <bradshaw.hyrum@gmail.com>
2025-05-13 17:11:48 -0600

    Update file structure and folder organization

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Creators from './pages/Creators';
+import Athletes from './pages/Athletes';
+import Error from './pages/Error';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/creators" element={<Creators />} />
+        <Route path="/athletes" element={<Athletes />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
